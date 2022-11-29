7bc375bf9313f08d5000a46b496400f2619e8401
Trishane <trishaneww@gmail.com>
2022-11-29 02:07:55 -0800

    restructured gloabl statistics

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import millify from 'millify';
-import { Typography, Row, Col, Statistic } from 'antd';
+import { Typography} from 'antd';
 import { Link } from 'react-router-dom';
-import {PlusOutlined } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import Cryptocurrencies from './Cryptocurrencies';
 import News from './News';
@@ -19,48 +19,48 @@ const Homepage = () => {
   return (
     <>
       <div>
-        <div class="global-stats" >
+        <div className="global-stats" >
         <div className="space">
           <div>
-            <p class="title">Cryptocurrencies</p>
-            <p class="num">{globalStats.total}</p>
+            <p className="title">Cryptocurrencies</p>
+            <p className="num">{globalStats.total}</p>
           </div>
         </div>
         <div className="space">
-          <div class="ban">
-            <p class="title">Exchanges</p>
-            <p class="num">{millify(globalStats.totalExchanges)}</p>
-          </div>
-        </div>
-        <div className="space">
-          <div>
-            <p class="title">Market Cap</p>
-            <p class="num">{`$${millify(globalStats.totalMarketCap)}`}</p>
+          <div className="ban">
+            <p className="title">Exchanges</p>
+            <p className="num">{millify(globalStats.totalExchanges)}</p>
           </div>
         </div>
         <div className="space">
           <div>
-            <p class="title">24h Volume</p>
-            <p class="num">{`$${millify(globalStats.total24hVolume)}`}</p>
+            <p className="title">Market Cap</p>
+            <p className="num">{`$${millify(globalStats.totalMarketCap)}`}</p>
           </div>
         </div>
         <div className="space">
           <div>
-            <p class="title">Markets</p>
-            <p class="num">{millify(globalStats.totalMarkets)}</p>
+            <p className="title">24h Volume</p>
+            <p className="num">{`$${millify(globalStats.total24hVolume)}`}</p>
+          </div>
+        </div>
+        <div className="space">
+          <div>
+            <p className="title">Markets</p>
+            <p className="num">{millify(globalStats.totalMarkets)}</p>
           </div>
         </div>
         </div>
       </div>
 
       <div className="home-heading-container">
-        <h1 class="home-title">Top 10 Cryptos In The World</h1>
-        <Title level={3} className="show-more"><Link to="/cryptocurrencies">{<PlusOutlined />}</Link></Title>
+        <h1 className="home-title">Top 10 Cryptos In The World</h1>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies"><PlusOutlined /></Link></Title>
       </div>
       <Cryptocurrencies simplified />
       <div className="home-heading-container">
-        <h1 class="home-title">Latest Crypto News</h1>
-        <Title level={3}><Link to="/news">{<PlusOutlined />}</Link></Title>
+        <h1 className="home-title">Latest Crypto News</h1>
+        <Title level={3}><Link to="/news"><PlusOutlined /></Link></Title>
       </div>
       <News simplified />
     </>
