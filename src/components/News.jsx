edf05339a3cfe11a7b2cef48a8ad771d66351ddb
Trishane <trishaneww@gmail.com>
2022-11-29 02:15:37 -0800

    renders deployable news articlecards

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import { Select, Row, Col} from 'antd';
 import moment from 'moment';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
@@ -7,8 +7,6 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import Loader from './Loader';
 
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
-
-const { Text, Title } = Typography;
 const { Option } = Select;
 
 const News = ({ simplified }) => {
@@ -37,20 +35,20 @@ const News = ({ simplified }) => {
       )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
-          <div class="news-container">
-          <div class="news-border">
-          <div class="crypto-carddd">
+          <div className="news-container">
+          <div className="news-border">
+          <div className="crypto-carddd">
             <a href={news.url} target="_blank" rel="noreferrer">
-              <div class="news-image-container">
-                <h1 class="news-title" level={4}>{news.name}</h1>
-                <img class="news-image" src={news?.image?.thumbnail?.contentUrl || demoImage} />
+              <div className="news-image-container">
+                <h1 className="news-title" level={4}>{news.name}</h1>
+                <img className="news-image" src={news?.image?.thumbnail?.contentUrl || demoImage} />
               </div>
-              <p class="news-desc">{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
-              <div class="news-card-stats">
-                    <img class="news-outlet-image" src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage}  />
-                    <p class="reference-title">{moment(news.datePublished).startOf('ss').fromNow()}</p>
+              <p className="news-desc">{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
+              <div className="news-card-stats">
+                    <img className="news-outlet-image" src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage}  />
+                    <p className="reference-title">{moment(news.datePublished).startOf('ss').fromNow()}</p>
               </div>
-              {/* <p class="card-title">{news.provider[0]?.name}</p> */}
+              {/* <p className="card-title">{news.provider[0]?.name}</p> */}
             </a>
           </div>
           </div>
