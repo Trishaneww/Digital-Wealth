3d8be68d7769ab5ebc8167b98989bb3edd753ccd
Trishane <trishaneww@gmail.com>
2022-11-29 02:13:36 -0800

    active Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined, LoginOutlined, LogoutOutlined, StarOutlined } from '@ant-design/icons';
+import { Button, Menu, Avatar } from 'antd';
+import { HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined, LoginOutlined, LogoutOutlined, StarOutlined } from '@ant-design/icons';
 
 import icon from '../images/digital-wealth.png';
 
@@ -29,26 +28,25 @@ const Navbar = () => {
   }, [screenSize]);
 
   return (
-    <div className={darkMode ? "dark-mode" : "nav-container"}>
-      <div className="logo-container">
-        <Avatar src={icon} size="large" />
-        <a class ='logo' href="/">Digital Wealth</a>
-        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+    <div className={darkMode ? 'dark-mode' : 'nav-container'}>
+      <div className='logo-container'>
+        <Avatar src={icon} size='large' />
+        <a className ='logo' href='/'>Digital Wealth</a>
+        <Button className ='menu-control-container' onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
       </div>
       {activeMenu && (
       <Menu className='menu'>
-          <a class ='a' href="/">{<HomeOutlined />} Home</a>
-          <a class ='a' href="/cryptocurrencies">{<FundOutlined />} Cryptocurrencies</a>
-          <a class ='a' href="/news">{<BulbOutlined />} News</a>
-          <a class ='a' href="/news">{<StarOutlined />} WatchList</a>
-          <a class ='a' href="/news">{<LoginOutlined />} Login</a>
-          <a class ='a' href="/news">{<LogoutOutlined />} Logout</a>
+          <a className ='a' href='/'> {<HomeOutlined />} Home</a>
+          <a className ='a' href='/cryptocurrencies'> {<FundOutlined />} Cryptocurrencies</a>
+          <a className ='a' href='/news'> {<BulbOutlined />} News</a>
+          <a className ='a' href='/news'> {<StarOutlined />} WatchList</a>
+          <a className ='a' href='/news'> {<LoginOutlined />} Login</a>
+          <a className ='a' href='/news'> {<LogoutOutlined />} Logout</a>
           
       </Menu>
       )}
     </div>
   );
 };
-
 
 export default Navbar;
